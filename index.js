ed0c3b1c001062f20ec74735caf1c2fb0624a15f
Michelle Caraballo <caraballomp@yahoo.com>
2022-03-05 15:03:41 -0800

    commit tree 811bed3f59462fdf3405df58775c6fe449974312

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (drivers){
+  return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(-2);
+  }
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(x){
+  return function(){
+    return x*x;
+  }}
+
+// let fareDoubler = createFareMultiplier();
+// fareDoubler = x => x*2;
+
+const doubler = createFareMultiplier();
+const fareDoubler = doubler => doubler * 2;
+
+
+const tripler = createFareMultiplier();
+const fareTripler = tripler => tripler * 3;
+
+
+function selectDifferentDrivers(arrayOfDrivers, drive){
+  return drive(arrayOfDrivers);
+}
+
+
+  // returnLastTwoDrivers
+// }
+
+
+
+//   function fareDoubler(x){
+//   return function(){
+//     return x*2;
+//   }
+
+
+//   const selectingDrivers = function (){
+// return ([returnFirstTwoDrivers (drivers)]);
+//     return ([returnLastTwoDrivers(drivers)])}
+
+// function selectingDrivers(array){
+//   const array =[`returnFirstTwoDrivers(), returnLastTwoDrivers()`];
+
+// }
